75a0f3c0b929a7154b018f3d1826e24ea559fed0
luk skywaka <luk@lukUb.D>
2021-11-18 17:16:51 +0000

    added labs test for factory pattern as DI

--- labs/streams/Program.cs
+++ labs/streams/Program.cs
@@ -14,6 +14,56 @@
     {
         internal static async Task Main(string[] args)
         {
+            // FactoryPatternInjection();
+
+            await NonNanoConsumer();
+
+        }
+
+        private static void FactoryPatternInjection()
+        {
+            var services = new ServiceCollection()
+                        .AddSingleton<Func<int, Constr>>(sp => @int => new Constr(@int, sp.GetRequiredService<IConstrInt>()))
+                        .AddSingleton<IConstrInt, ConstrInt>();
+
+            var provider = services
+            .BuildServiceProvider();
+
+            var c1 = provider
+            .GetService<Func<int, Constr>>()
+            (1);
+
+            var c2 = provider
+            .GetService<Func<int, Constr>>()
+            (2);
+
+
+            Console.WriteLine($"c1.Ref = {c1.Ref}\tc1.Name = {c2.ConstrInt.Name}\nc2.Ref = {c2.Ref}\tc2.Name = {c2.ConstrInt.Name}\n");
+        }
+
+        class Constr
+        {
+
+            public int Ref { get; }
+            public IConstrInt ConstrInt { get; }
+
+            public Constr(int @ref, IConstrInt constrInt)
+            {
+                Ref = @ref;
+                ConstrInt = constrInt;
+            }
+        }
+
+        interface IConstrInt
+        {
+            string Name => "Ola Inj";
+        }
+
+        class ConstrInt : IConstrInt { }
+
+
+        private static async Task NonNanoConsumer()
+        {
             var container = new ServiceCollection().AttachFlow().BuildServiceProvider();
 
             IFlow flow1 = container.GetRequiredService<IFlow>();
@@ -42,78 +92,79 @@
             await whispers;
             await instruction;
             await whisper;
-
-            // var microABus = new Bus();
-            // microABus.AddOracle<Bar>();
-
-            // var microBBus = new Bus();
-
-            // var flow = new Flow(new[] { microABus, microBBus });
-
-            // flow.WireUp();
-
-            // var nanoA = new MyNanoServiceA();
-            // var nanoB = new MyNanoServiceB();
-            // var nanoA2 = new MyNanoServiceA2();
-            // var nanoB2 = new MyNanoServiceB2();
-
-            // nanoA.Connect(microABus).Subscribe();
-            // nanoA2.Connect(microABus).Subscribe();
-
-            // nanoB.Connect(microBBus).Subscribe();
-            // nanoB2.Connect(microBBus).Subscribe();
-
-            // //flow.SendCommand(new FooCommand());
-            // flow.SendCommand(new UpdateSomethingCommand());
-
-            // Console.WriteLine("Hello World!");
         }
     }
 
-    //     public class Flow
-    //     {
-    //         private readonly List<IMicroBus> _buses = new(); //Created after assemblies scanning
 
-    //         public Flow(IEnumerable<IMicroBus> buses) => _buses = new List<IMicroBus>(buses);
-
-    //         //1 - Scan assemblies
-    //         //2 - Create one Bus per each MicroService
-    //         //3 - Subscribe To Bus Streams / Commands Sent
-    //         //4 - Invoke connect passing the respective bus
-
-    //         public void WireUp()
-    //         {
-    //             _buses
-    //                 .Cast<ICommandBus>()
-    //                 .Select(bus => bus.CommandsSent)
-    //                 .Merge()
-    //                 .Do(command => _buses.Cast<ICommandBus>().ToList().ForEach(bus => bus.RouteCommand(command)))
-    //                 .Subscribe();
-
-    //             _buses
-    //                 .Cast<IEventbus>()
-    //                 .Select(bus => bus.EventsSent)
-    //                 .Merge()
-    //                 .Do(@event => _buses.Cast<IEventbus>().ToList().ForEach(bus => bus.RouteEvent(@event)))
-    //                 .Subscribe();
-
-    //             var allStreams = _buses
-    //                 .Cast<IStreamSource>()
-    //                 .SelectMany(streamSource => streamSource.LocalStreams)
-    //                 .Select(x => (x.Key, x.Value))
-    //                 .ToDictionary(x => x.Key, x => x.Value);
-
-    //             _buses
-    //                  .Cast<IStreamSource>()
-    //                  .ToList()
-    //                  .ForEach(bus => bus.AllStreams.AddRange(allStreams));
-    //         }
-
-    //         public void SendCommand(ICommand command) =>
-    //             _buses
-    //                 .Cast<ICommandBus>()
-    //                 .ToList()
-    //                 .ForEach(bus => bus.RouteCommand(command));
-    //     }
 }
+// var microABus = new Bus();
+// microABus.AddOracle<Bar>();
 
+// var microBBus = new Bus();
+
+// var flow = new Flow(new[] { microABus, microBBus });
+
+// flow.WireUp();
+
+// var nanoA = new MyNanoServiceA();
+// var nanoB = new MyNanoServiceB();
+// var nanoA2 = new MyNanoServiceA2();
+// var nanoB2 = new MyNanoServiceB2();
+
+// nanoA.Connect(microABus).Subscribe();
+// nanoA2.Connect(microABus).Subscribe();
+
+// nanoB.Connect(microBBus).Subscribe();
+// nanoB2.Connect(microBBus).Subscribe();
+
+// //flow.SendCommand(new FooCommand());
+// flow.SendCommand(new UpdateSomethingCommand());
+
+// Console.WriteLine("Hello World!");
+
+
+//     public class Flow
+//     {
+//         private readonly List<IMicroBus> _buses = new(); //Created after assemblies scanning
+
+//         public Flow(IEnumerable<IMicroBus> buses) => _buses = new List<IMicroBus>(buses);
+
+//         //1 - Scan assemblies
+//         //2 - Create one Bus per each MicroService
+//         //3 - Subscribe To Bus Streams / Commands Sent
+//         //4 - Invoke connect passing the respective bus
+
+//         public void WireUp()
+//         {
+//             _buses
+//                 .Cast<ICommandBus>()
+//                 .Select(bus => bus.CommandsSent)
+//                 .Merge()
+//                 .Do(command => _buses.Cast<ICommandBus>().ToList().ForEach(bus => bus.RouteCommand(command)))
+//                 .Subscribe();
+
+//             _buses
+//                 .Cast<IEventbus>()
+//                 .Select(bus => bus.EventsSent)
+//                 .Merge()
+//                 .Do(@event => _buses.Cast<IEventbus>().ToList().ForEach(bus => bus.RouteEvent(@event)))
+//                 .Subscribe();
+
+//             var allStreams = _buses
+//                 .Cast<IStreamSource>()
+//                 .SelectMany(streamSource => streamSource.LocalStreams)
+//                 .Select(x => (x.Key, x.Value))
+//                 .ToDictionary(x => x.Key, x => x.Value);
+
+//             _buses
+//                  .Cast<IStreamSource>()
+//                  .ToList()
+//                  .ForEach(bus => bus.AllStreams.AddRange(allStreams));
+//         }
+
+//         public void SendCommand(ICommand command) =>
+//             _buses
+//                 .Cast<ICommandBus>()
+//                 .ToList()
+//                 .ForEach(bus => bus.RouteCommand(command));
+//     }
--- lib/core/nflow.core/Bootstrap/BootstrapRegistry.cs
+++ lib/core/nflow.core/Bootstrap/BootstrapRegistry.cs
@@ -1,11 +1,16 @@
 namespace nflow.core
 {
+    using System;
+    using System.Collections.Generic;
     using Microsoft.Extensions.DependencyInjection;
 
     internal class BootstrapRegistry : Registry
     {
         public BootstrapRegistry()
         {
+
+            // AddFactories();
+
             this.AddSingleton<IFlow, Flow>();
 
             this.AddSingleton<IStreamsResolver, StreamsResolver>();
@@ -13,7 +18,14 @@ namespace nflow.core
             this.AddSingleton<INanosResolver, NanosResolver>();
             this.AddSingleton<IMicrosResolver, MicrosResolver>();
 
+            this.AddSingleton<IEnumerable<IMicro>>(sp => sp.GetRequiredService<IMicrosResolver>().All);
+            this.AddSingleton<IEnumerable<INano>>(sp => sp.GetRequiredService<INanosResolver>().All);
+
+
+
             this.AddSingleton<FlowBus>();
         }
+
+        // private void AddFactories() => this.AddSingleton<Func<Registry, IMicro>>(sp);
     }
 }
